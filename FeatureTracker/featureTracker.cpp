67e54fc565855440cfe2361f5e31e0acbbee9c5e
spav <sumanthpavuluri@gmail.com>
2017-03-14 20:32:23 -0700

    implemetation of Feature tracker

--- FeatureTracker/featureTracker.cpp
+++ FeatureTracker/featureTracker.cpp
@@ -1,108 +1,111 @@
-﻿
 #include <iostream>
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/features2d/features2d.hpp>
-#include <opencv2/video/tracking.hpp>
+#include "FeatureTracker.h"
 
-void writeToFile(cv::FileStorage &fs, std::string name,
-	std::vector<std::vector<cv::Point2f>> &points);
+//ft::Output ft::FeatureTracker::track(cv::Mat &frame,
+						//int & frame_width, int & frame_height){
 
-int main()
-{
-	// Open the video file
-	cv::VideoCapture capture("../shark.mp4"); //bike.avi, shark.mp4
-												   // check if video successfully opened
-	if (!capture.isOpened())
-		return 1;
-	cv::Mat frame; // current video frame
-	cv::namedWindow("Tracked Features");
-	cv::Mat gray, gray_prev;
-	std::vector<cv::Point2f> points[2]; //points from previous and current frame
-	std::vector<cv::Point2f> features;
-	const int MAX_COUNT = 500; //max number of corners to return
-	cv::Size winSize(10, 10);
-	cv::TermCriteria termcrit(CV_TERMCRIT_ITER | CV_TERMCRIT_EPS,
-		20, 0.03);
-	bool firstframe = true;
+ft::Output ft::FeatureTracker::track(cv::VideoCapture &capture) {
+	Output out;
+	cv::Mat frame;
 	int frame_width = capture.get(CV_CAP_PROP_FRAME_WIDTH);
 	int frame_height = capture.get(CV_CAP_PROP_FRAME_HEIGHT);
-	cv::VideoWriter video("shark_ft1.avi", CV_FOURCC('M', 'J', 'P', 'G'),
+	cv::Mat output;
+	cv::VideoWriter video("output_tracks.avi", CV_FOURCC('M', 'J', 'P', 'G'),
 		10, cv::Size(frame_width, frame_height), true);
-
-	cv::FileStorage fs("Tracked points.yml", cv::FileStorage::WRITE);
-	// for all frames in video
+	cv::namedWindow("Tracked Features");
 	for (;;) {
-
-		// read next frame if any
+		// convert to gray-level image
+		//	read next frame if any
 		if (!capture.read(frame))
 			break;
-
 		cv::cvtColor(frame, gray, CV_BGR2GRAY);
-		if (firstframe)
+		frame.copyTo(output);
+
+		// 1. if new feature points must be added
+		if (addNewPoints())
 		{
-			cv::goodFeaturesToTrack(gray, features, MAX_COUNT, 0.01, 10, cv::Mat(), 3, 0, 0.04);
-			cv::cornerSubPix(gray, features, winSize, cv::Size(-1, -1), termcrit);
+			// detect feature points
+			detectFeaturePoints();
+			// add the detected features to the currently tracked features
 			points[0].insert(points[0].end(), features.begin(), features.end());
-			firstframe = false;
+			initial.insert(initial.end(), features.begin(), features.end());
 		}
 
-		{
-			std::vector<uchar> status;
-			std::vector<float> err;
-			if (gray_prev.empty())
-				gray.copyTo(gray_prev);
-			cv::calcOpticalFlowPyrLK(gray_prev, gray, points[0], points[1], status, err, winSize, 3, termcrit, 0);
-			size_t i, k;
-			for (i = k = 0; i < points[1].size(); i++)
-			{
-				if (!status[i])
-					continue;
-				if ((abs(points[0][i].x - points[1][i].x) + (abs(points[0][i].y - points[1][i].y)) < 2))
-					continue;
+		// for first image of the sequence
+		if (gray_prev.empty())
+			gray.copyTo(gray_prev);
 
-				cv::line(frame, points[0][i], points[1][i], cv::Scalar(0, 0, 255));
+		// 2. track features
+		cv::calcOpticalFlowPyrLK(gray_prev, gray, // 2 consecutive images
+			points[0], // input point position in first image
+			points[1], // output point postion in the second image
+			status,    // tracking success
+			err);      // tracking error
+
+					   // 2. loop over the tracked points to reject the undesirables
+		int k = 0;
+		for (int i = 0; i < points[1].size(); i++) {
+
+			// do we keep this point?
+			if (acceptTrackedPoint(i)) {
+
+				// keep this point in vector
+				initial[k] = initial[i];
 				points[1][k++] = points[1][i];
-				cv::circle(frame, points[1][i], 3, cv::Scalar(0, 255, 0), -1, 8);
 			}
-			points[1].resize(k);
 		}
-		if (points[1].size() <= 10)
-			firstframe = true;
+
+		// eliminate unsuccesful points
+		points[1].resize(k);
+		initial.resize(k);
+
+		// 3. handle the accepted tracked points
+		out.output_video = handleTrackedPoints(frame, output);
+
+		// 4. current points and image become previous ones
 		std::swap(points[1], points[0]);
 		cv::swap(gray_prev, gray);
-
 		if (cv::waitKey(33) >= 0)
 		{
-			cv::imshow("Tracked Features", frame);
+			cv::imshow("Tracked Features", output);
 		}
 
-		//write frame to output file
-		{
-			video.write(frame);
-		}
-		std::vector<std::vector<cv::Point2f>> p = { points[0], points[1] };
+		//save output video
+		video.write(out.output_video);
 
-		//write to a file
-		writeToFile(fs, "TF", p);
-		//cv::write(fs, "TF", p);
-		p.clear();
+		////write tracks to a file
+		//cv::FileStorage fs("Tracked_Points.yml", cv::FileStorage::WRITE);
+		////out.fs = writeToFile(fs, "TF", p);
+		//std::vector<std::vector<cv::Point2f>> p = { points[0], points[1] };
+		//writeToFile(fs, "TF", p);
+		//p.clear();
 	}
 
-	//close the yml file
-	fs.release();
-	// Close the video file
-	capture.release();
-
-	cv::waitKey();
-
-
+	return out;
 }
 
+bool ft::FeatureTracker::acceptTrackedPoint(int i) {
+	return status[i] &&
+		// if point has moved
+		(abs(points[0][i].x - points[1][i].x) +
+		(abs(points[0][i].y - points[1][i].y)) > 2);
+}
 
-void writeToFile(cv::FileStorage &fs, std::string name, std::vector<std::vector<cv::Point2f>> &p)
+cv::Mat ft::FeatureTracker::handleTrackedPoints(cv::Mat &frame, cv::Mat &output) {
+
+	// for all tracked points
+	for (int i = 0; i < points[1].size(); i++) {
+
+		// draw line and circle
+		cv::line(output, initial[i], points[1][i], cv::Scalar(0, 0, 255));
+		cv::circle(output, points[1][i], 3, cv::Scalar(0, 255, 0), -1);
+	}
+	return output;
+}
+
+void ft::FeatureTracker::writeToFile(cv::FileStorage &fs, std::string name, std::vector<std::vector<cv::Point2f>> &p)
 {
+	//cv::FileStorage fs("Tracked_Points.yml", cv::FileStorage::WRITE);
 	fs << name;
 	fs << "{";
 	for (int i = 0; i < p.size(); i++)
